0b54716b5c19937e5c372dce339edbcf03e390bf
Suganya1996 <st.natarajan.003@gmail.com>
2022-05-21 21:14:57 +0530

    new code

---
+++ src/datatype/com/Client.java
@@ -0,0 +1,60 @@
+package datatype.com;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		//datatype VaribleName = Variablevalue
+		
+		byte age = 25;
+		System.out.println(age);
+		
+		short year = 2022;
+		System.out.println("current year is "+year);
+		
+		int p = 880708881;
+		System.out.println(p);
+		System.out.println(p+100);  // addition
+		System.out.println(p+"100");  // Concatenation
+		
+		long phone = 8807088813l;
+		System.out.println(phone);
+		
+		float salary = 50000.20f;
+		System.out.println(salary);
+		
+		char gender = 'M';
+		System.out.println(gender);
+		
+		boolean b = false;
+		System.out.println(b);
+		
+		String email = "Gmail @123";
+		System.out.println(email);
+		
+		float f = 20/3f;
+		System.out.println(f);
+		
+		double d = 20/3d;
+		System.out.println(d);
+		
+		
+		
+		
+		
+				
+		
+				
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
